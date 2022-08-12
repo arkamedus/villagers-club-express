3f63eee216fd1b316ddf0516d6d525e7e7ca14e0
Gordon Goodrum <stopgordy@gmail.com>
2022-08-12 02:33:40 -0500

    Associated Data

---
+++ data/recipes.js
@@ -0,0 +1,52 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CardColor;
+(function (CardColor) {
+    CardColor["Beige"] = "beige";
+    CardColor["Blue"] = "blue";
+    CardColor["Brick"] = "brick";
+    CardColor["Brown"] = "brown";
+    CardColor["Cream"] = "cream";
+    CardColor["DarkGray"] = "dark gray";
+    CardColor["Gold"] = "gold";
+    CardColor["Green"] = "green";
+    CardColor["LightGray"] = "light gray";
+    CardColor["Orange"] = "orange";
+    CardColor["Pink"] = "pink";
+    CardColor["Purple"] = "purple";
+    CardColor["Red"] = "red";
+    CardColor["Silver"] = "silver";
+    CardColor["White"] = "white";
+    CardColor["Yellow"] = "yellow";
+})(CardColor = exports.CardColor || (exports.CardColor = {}));
+var Category;
+(function (Category) {
+    Category["Equipment"] = "Equipment";
+    Category["Floors"] = "Floors";
+    Category["Housewares"] = "Housewares";
+    Category["Miscellaneous"] = "Miscellaneous";
+    Category["Other"] = "Other";
+    Category["Rugs"] = "Rugs";
+    Category["Tools"] = "Tools";
+    Category["WallMounted"] = "Wall-mounted";
+    Category["Wallpaper"] = "Wallpaper";
+})(Category = exports.Category || (exports.Category = {}));
+var ExchangeCurrency;
+(function (ExchangeCurrency) {
+    ExchangeCurrency["NookMiles"] = "Nook Miles";
+})(ExchangeCurrency = exports.ExchangeCurrency || (exports.ExchangeCurrency = {}));
+var SourceSheet;
+(function (SourceSheet) {
+    SourceSheet["Recipes"] = "Recipes";
+})(SourceSheet = exports.SourceSheet || (exports.SourceSheet = {}));
+var VersionAdded;
+(function (VersionAdded) {
+    VersionAdded["The100"] = "1.0.0";
+    VersionAdded["The110"] = "1.1.0";
+    VersionAdded["The120"] = "1.2.0";
+    VersionAdded["The130"] = "1.3.0";
+    VersionAdded["The140"] = "1.4.0";
+    VersionAdded["The150"] = "1.5.0";
+    VersionAdded["The160"] = "1.6.0";
+})(VersionAdded = exports.VersionAdded || (exports.VersionAdded = {}));
+//# sourceMappingURL=recipes.js.map
